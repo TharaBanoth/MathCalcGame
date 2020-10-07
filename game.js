4438573bf6e34416e5716f8b5417b21cb1bea737
Saiteja Vemula <saitejavemula@gmail.com>
2020-10-07 18:53:40 +0530

    Fixed the focus() issue for input element, and the "Play Again" button

--- game.js
+++ game.js
@@ -9,102 +9,118 @@ const statement = document.querySelector(".statement");
 const gameStatus = document.querySelector(".game-status");
 const reset = document.querySelector(".overlay button");
 
+
+userAnswer.focus();
+
+
 let state = {
-    wrongAnswers : 0,
-    questionsLeft : 10
-}
+  wrongAnswers: 0,
+  questionsLeft: 10
+};
 
 // Function Declarations
 function generateNumber(x) {
-    return Math.floor(Math.random() * x + 1);
+  return Math.floor(Math.random() * x + 1);
 }
 
 function generateQuestion() {
-    return {
-        value1 : generateNumber(10),
-        value2 : generateNumber(10),
-        operator : ['+', '-', 'x'][generateNumber(2)]
-    }
+  let a = generateNumber(10);
+  let b = generateNumber(10);
+  if (b > a) {
+    a = a + b;
+    b = a - b;
+    a = a - b;
+  }
+  return {
+    value1: a,
+    value2: b,
+    operator: ["+", "-", "x"][generateNumber(2)]
+  };
 }
 
 function updateQuestion() {
-    state.currentQuestion = generateQuestion();
-    let q = state.currentQuestion;
-    question.textContent = `${q.value1} ${q.operator} ${q.value2}`;
+  state.currentQuestion = generateQuestion();
+  let q = state.currentQuestion;
+  question.textContent = `${q.value1} ${q.operator} ${q.value2}`;
 }
 
 function formSub(e) {
-    e.preventDefault();
+  e.preventDefault();
+  userAnswer.focus();
+  
+  let q = state.currentQuestion;
+  let correctAnswer;
 
-    let q = state.currentQuestion;
-    let correctAnswer;
+  if (q.operator === "+") correctAnswer = q.value1 + q.value2;
+  if (q.operator === "-") correctAnswer = q.value1 - q.value2;
+  if (q.operator === "x") correctAnswer = q.value1 * q.value2;
 
-    if(q.operator === '+') correctAnswer =  q.value1 +  q.value2;
-    if(q.operator === '-') correctAnswer =  q.value1 -  q.value2;
-    if(q.operator === 'x') correctAnswer =  q.value1 *  q.value2;
-    
-    form.focus();
+  // form.focus();
 
-    if(correctAnswer === userAnswer.valueAsNumber) {
-        state.questionsLeft -= 1;
-        statement.textContent = `You have ${state.questionsLeft} questions to answer, and can answer ${3-state.wrongAnswers} wrongs.`;
-        progressBar.style.transform = `scaleX(0.${10 - state.questionsLeft})`;
+  if (correctAnswer === userAnswer.valueAsNumber) {
+    state.questionsLeft -= 1;
+    statement.innerHTML = `You have ${
+      state.questionsLeft
+    } questions to answer<br>and can answer ${3 - state.wrongAnswers} wrongs.`;
+    progressBar.style.transform = `scaleX(0.${10 - state.questionsLeft})`;
 
-        if(state.questionsLeft === 0) {
-            reset.focus();
-            gameOver(true);
-        }
-
-    } else {
-        state.wrongAnswers += 1;
-        question.classList.add("error");
-        statement.textContent = `You have ${state.questionsLeft} questions to answer, and can answer ${3-state.wrongAnswers} wrongs.`;
-        setTimeout(() => { question.classList.remove("error"); }, 550);
-        if(state.wrongAnswers === 3) {
-            reset.focus();
-            gameOver(false)
-        }
+    if (state.questionsLeft === 0) {
+      reset.focus();
+      gameOver(true);
     }
-    userAnswer.value = '';
-    updateQuestion();
+  } else {
+    state.wrongAnswers += 1;
+    question.classList.add("error");
+    statement.innerHTML = `You have ${
+      state.questionsLeft
+    } questions to answer<br>and can answer ${3 - state.wrongAnswers} wrongs.`;
+    setTimeout(() => {
+      question.classList.remove("error");
+    }, 550);
+    if (state.wrongAnswers === 3) {
+      reset.focus();
+      gameOver(false);
+    }
+  }
+  userAnswer.value = "";
+  updateQuestion();
 }
 
 function gameOver(won) {
-    form.blur();
-    userAnswer.blur()
+  form.blur();
+  userAnswer.blur();
 
-    reset.focus();
-    bg.classList.add("blur")
-    popUp.classList.remove("hid");
-    progressBar.style.transform = `scaleX(0.0)`;
+  
+  bg.classList.add("blur");
+  popUp.classList.remove("hid");
+  progressBar.style.transform = `scaleX(0.0)`;
 
-
-    if(won) {
-        gameStatus.textContent = `Congratulations! You've Won the Game.`;
-    } else {
-        gameStatus.textContent = `Sorry, You've Lost the Game.`;
-    }
+  if (won) {
+    gameStatus.textContent = `Congratulations! You've Won the Game.`;
+  } else {
+    gameStatus.textContent = `Sorry, You've Lost the Game.`;
+  }
+  reset.focus();
 }
 
 function resetGame() {
-    state.questionsLeft = 10;
-    state.wrongAnswers = 0;
+  state.questionsLeft = 10;
+  state.wrongAnswers = 0;
 
-    bg.classList.remove("blur")
-    popUp.classList.add("hid");
-    progressBar.style.transform = `scaleX(0.0)`;
-    statement.textContent = `You have ${state.questionsLeft} questions to answer, and can answer ${3-state.wrongAnswers} wrongs.`;
-    updateQuestion();
+  bg.classList.remove("blur");
+  popUp.classList.add("hid");
+  progressBar.style.transform = `scaleX(0.0)`;
+  statement.innerHTML = `You have ${
+    state.questionsLeft
+  } questions to answer<br>and can answer ${3 - state.wrongAnswers} wrongs.`;
+  updateQuestion();
+  
+  userAnswer.focus()
 }
-
-
 
 // Events
 form.addEventListener("submit", formSub);
 reset.addEventListener("click", resetGame);
-
-
-
 
 // Function Calls
 updateQuestion();
